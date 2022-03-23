6de0bbf325261dcdca9103c2a9cc099e42b0de85
Aleksi Jarva <aleksi.jarva@mogroup.com>
2022-03-23 14:34:09 +0200

    3.5: puhelinluettelon backend step5

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express")
 const app = express()
+app.use(express.json())
 
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
 
 let persons = [
     {
@@ -53,7 +58,22 @@ app.delete("/api/persons/:id", (request, response) =>{
     response.status(204).end()
 })
 
-const PORT = 3001
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+app.post("/api/persons/", (request, response) => {
+    const body = request.body
+
+    if(!body.name || !body.number) {
+        return response.status(400).json({
+            error: "content missing"
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: Math.floor(Math.random() * 100)
+    }
+
+    persons = persons.concat(person)
+    response.json(person)
 })
+
