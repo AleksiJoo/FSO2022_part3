2eccc4e75818cd9fc28e26a248343cb8300fb609
Aleksi Jarva <aleksijoonatan@gmail.com>
2022-04-07 10:26:17 +0300

    Add Procfile for heroku

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(morgan(":method :url :status :response-time ms :body"))
 
 morgan.token("body", (req, res) => JSON.stringify(req.body))
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
