4483ec55432a6a429d8f9d859d71bfe75dc0fbef
Aleksi Jarva <aleksi.jarva@mogroup.com>
2022-07-26 13:03:12 +0300

    3.16: puhelinluettelo ja tietokanta, step4

--- index.js
+++ index.js
@@ -55,17 +55,19 @@ app.get("/info", (request, response) => {
     
 })
 
-app.get("/api/persons/:id", (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(p => p.id === id)
-
-    if (person) {
-        response.json(person)
-    } else {
-    response.status(404).end()}
+app.get("/api/persons/:id", (request, response, next) => {
+    Person.findById(request.params.id)
+    .then(person => {
+        if (person) {
+            response.json(person)
+        } else {
+            response.status(404).end()
+        }
+    })
+    .catch(error => next(error))
 })
 
-app.delete("/api/persons/:id", (request, response) =>{
+app.delete("/api/persons/:id", (request, response, next) =>{
     Person.findByIdAndRemove(request.params.id)
     .then(result => {
         response.status(204).end()
